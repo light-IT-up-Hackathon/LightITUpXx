86f6d6abe9706459196b62c1b58ef41c8f5f4900
liz0904 <liz09045@naver.com>
2021-08-12 00:30:11 +0900

    Login/Join/Coupon xml.etc

---
+++ app/src/main/java/com/example/lightitupxx/activity/CouponActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lightitupxx.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lightitupxx.R
+
+class CouponActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_coupon)
+
+    }
+}
