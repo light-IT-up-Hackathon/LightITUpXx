017a68e9b7803defc35ccdcc6967c4592852842a
SoA-Lee <llsa0927@naver.com>
2021-08-19 05:06:11 +0900

    [Add]HomeMasterActivity

---
+++ app/src/main/java/com/example/lightitupxx/activity/HomeMasterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lightitupxx.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lightitupxx.R
+
+class HomeMasterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_master)
+    }
+}
--- app/src/main/java/com/example/lightitupxx/activity/member/LoginActivity.kt
+++ app/src/main/java/com/example/lightitupxx/activity/member/LoginActivity.kt
@@ -10,10 +10,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lightitupxx.R
 import com.example.lightitupxx.activity.HomeActivity
+import com.example.lightitupxx.activity.HomeMasterActivity
 import com.example.lightitupxx.api.userClass
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import io.realm.exceptions.RealmMigrationNeededException
+import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
 
 class LoginActivity: AppCompatActivity() {
@@ -39,16 +41,24 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        rg1.setOnCheckedChangeListener { radioGroup, i ->
+            when(i){
+                R.id.radioButton1 ->
+                    btn_login.setOnClickListener {
+                        login_master()
+                    }
+                R.id.radioButton2 -> btn_login.setOnClickListener {
+                    login()
+                }
+            }
+        }
+
         etv_login_id=findViewById(R.id.etv_login_id)
         etv_login_pwd=findViewById(R.id.etv_login_pwd)
         btn_login=findViewById(R.id.btn_login)
         tv_joining=findViewById(R.id.tv_joining)
 
         handler = Handler()
-
-        btn_login.setOnClickListener {
-            login()
-        }
 
         tv_joining.setOnClickListener {
             Toast.makeText(this,"회원가입 창으로 이동", Toast.LENGTH_SHORT).show()
@@ -73,6 +83,25 @@ class LoginActivity: AppCompatActivity() {
             Toast.makeText(this,"로그인 정보가 없거나 틀립니다.", Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun login_master(){
+        val id=etv_login_id.text.toString()
+        val pwd=etv_login_pwd.text.toString()
+
+        val person=loginRealm?.where(userClass::class.java)!!
+            .equalTo("id", id)
+            .equalTo("pwd", pwd)
+            .findFirst()
+
+        if(person!=null){
+            Toast.makeText(this,"로그인 성공", Toast.LENGTH_SHORT).show()
+            var intent=Intent(this, HomeMasterActivity::class.java)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"로그인 정보가 없거나 틀립니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         loginRealm?.close()
